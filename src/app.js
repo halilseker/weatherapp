562ae5fcf7fc82d7f932d4de24d8067bcae0c32d
halilseker <g141210082@sakarya.edu.tr>
2022-10-29 16:49:33 +0100

    Partials with handlebars S7L49

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
 
 console.log(__dirname);
 console.log(path.join(__dirname, '../public'));
@@ -7,11 +8,13 @@ const app = express();
 
 //Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public');
-const viewsPath = path.join(__dirname, '../templates');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
 //Setup handlbars engine and views location
 app.set('view engine', 'hbs');
 app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
 
 //Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
@@ -24,7 +27,11 @@ app.get('/about', (req, res) => {
   res.render('about', { title: 'About', name: 'HIS' });
 });
 app.get('/help', (req, res) => {
-  res.render('help', { message: 'Please contact', name: 'HIS' });
+  res.render('help', {
+    helpText: 'Please contact to',
+    title: 'Help',
+    name: 'HIS',
+  });
 });
 
 // app.get('/', (req, res) => {
