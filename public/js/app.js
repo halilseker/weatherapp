add43e30320eb6ad5832917f1bcf43f20f8010e4
halilseker <g141210082@sakarya.edu.tr>
2022-10-31 17:55:27 +0000

    Browser http requests with Fetch s8L57

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,10 @@
 console.log('Client side javascript code ');
+
+fetch('http://localhost:3000/weather?address=guarda').then((response) => {
+  response.json().then((data) => {
+    if (data.error) {
+      console.log('error occured: ', data.error);
+    }
+    console.log(data);
+  });
+});
