a73055ded7a1942ec1b6567a81d03fbcaf454bed
halilseker <g141210082@sakarya.edu.tr>
2022-10-31 22:29:09 +0000

    wiring up the user interface s8L59

--- public/js/app.js
+++ public/js/app.js
@@ -12,17 +12,28 @@ console.log('Client side javascript code ');
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
+
 weatherForm.addEventListener('submit', (event) => {
   event.preventDefault();
+
   const location = search.value;
+
+  messageOne.textContent = 'Loading...';
+  messageTwo.textContent = '';
+
   console.log('testing:', location);
   fetch(`http://localhost:3000/weather?address=${location}`).then(
     (response) => {
       response.json().then((data) => {
         if (data.error) {
           console.log('error occured: ', data.error);
+          messageOne.textContent = data.error;
+        } else {
+          messageOne.textContent = data.location;
+          messageTwo.textContent = data.forecast;
         }
-        console.log(data);
       });
     }
   );
