85718b0bd5f2341b9d191db692be17bee64a18b2
halilseker <g141210082@sakarya.edu.tr>
2022-10-31 18:39:58 +0000

    Creating a search form Fetch s8L58

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,29 @@
 console.log('Client side javascript code ');
 
-fetch('http://localhost:3000/weather?address=guarda').then((response) => {
-  response.json().then((data) => {
-    if (data.error) {
-      console.log('error occured: ', data.error);
+// fetch('http://localhost:3000/weather?address=guarda').then((response) => {
+//   response.json().then((data) => {
+//     if (data.error) {
+//       console.log('error occured: ', data.error);
+//     }
+//     console.log(data);
+//   });
+// });
+
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+
+weatherForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const location = search.value;
+  console.log('testing:', location);
+  fetch(`http://localhost:3000/weather?address=${location}`).then(
+    (response) => {
+      response.json().then((data) => {
+        if (data.error) {
+          console.log('error occured: ', data.error);
+        }
+        console.log(data);
+      });
     }
-    console.log(data);
-  });
+  );
 });
